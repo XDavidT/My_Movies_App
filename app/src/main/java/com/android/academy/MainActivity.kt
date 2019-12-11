7f6f5692d50d092b9375d0e69549701da1322433
David Tayar <xdavidt@gmail.com>
2019-12-11 10:16:50 +0200

    done exercise 2 :)

--- app/src/main/java/com/android/academy/MainActivity.kt
+++ app/src/main/java/com/android/academy/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.android.academy
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun moveToYoutubeLink(view : View){
+        val move_to_link = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=6ZfuNTqbHE8"))
+        move_to_link.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        move_to_link.setPackage("com.google.android.youtube")
+        startActivity(move_to_link)
+
     }
 }
