055a1ae1a5901ec134f292ac2f892a7a0804b35b
David Tayar <xdavidt@gmail.com>
2019-12-29 14:17:22 +0200

    Start using fragments

--- app/src/main/java/com/android/academy/details/details/MainActivity.kt
+++ app/src/main/java/com/android/academy/details/details/MainActivity.kt
@@ -6,45 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.android.academy.R
+import com.android.academy.fragments.MoviesFragment
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    val facts = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        facts.add("Captain America is confirmed to have a much larger role in Avengers 4.")
-        facts.add("When Thanos hurls the moon on Titan, he uses two Infinity Stones to do so: the Power Stone and Space Stone")
-        facts.add("Valkyrie and several other people escaped the Asgardian ship before Thanos starts wreaking havoc. Sadly, we still don’t know the fate of Korg, but my hunch is he’ll make a return in Avengers 4.")
-        facts.add("Kenneth Branagh, who directed the first Thor, does the voice for the distress call in the very beginning.")
-        facts.add("One of the early drafts of the script saw Thanos collect the Power Stone from Xandar, but the directors ultimately decided not to show him collecting each stone because it would be too repetitive.")
-        facts.add("Speaking of Xandar, Gamora was supposed to encounter Thanos there, but they eventually scrapped that and instead changed the location to Knowhere.")
-        facts.add("The directors included a lot of nods to Raiders of the Lost Ark, including a scene that sees Maw burn his hand when he reaches for Doctor Strange’s Time Stone.")
-        facts.add("Early drafts included backstory for the Black Order, but they were eventually cut because it would have made the movie too bloated.")
-        facts.add("The directors wanted to save Captain America until the battle on Wakanda but ultimately decided to have him appear in Scotland.")
-        facts.add("In order to explain what each Infinity Stone did, the directors made sure to show Thanos using them soon after he got a new one.")
-        facts.add("When we see Thanos turn Drax and Mantis into weird shapes, it was a nod to the Jim Starlin Infinity Gauntlet comics.")
-        facts.add("The directors weren’t sure about setting the final battle in Wakanda. But after the success of Black Panther, they knew they made the right choice.")
-        facts.add("The directors couldn’t figure out how to make Tony Stark beat Ebony Maw aboard the spaceship, so they looked to Raiders of the Lost Ark again as inspiration. Stark eventually wins by blowing a hole in the side of the spaceship, which sucks Maw out.")
-        facts.add("Nebula getting caught essentially allows Thanos to successfully obtain all the Infinity Stones, because she reveals to the Mad Titan that Gamora knew where the Soul Stone was.")
-        facts.add("The directors were initially going to show Thor’s new weapon, Stormbreaker, being forged by dragons. But they eventually settled on Eitri.")
-        facts.add("The role of Red Skull was necessary because the directors needed someone to explain how to obtain the Soul Stone.")
-        facts.add("Early drafts of the script included more of Thanos’ backstory, but the scenes were cut.")
-    }
+        val moviesFragment = MoviesFragment()
 
-    fun moveToYoutubeLink(){
-        val move_to_link = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=6ZfuNTqbHE8"))
-        move_to_link.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        move_to_link.setPackage("com.google.android.youtube")
-        startActivity(move_to_link)
-
-    }
-
-    fun popFunFact(){
-        val factIdx = Random.nextInt(0,facts.size)
-        val toastMassage = Toast.makeText(applicationContext,facts[factIdx],(Toast.LENGTH_LONG*2))
-        toastMassage.show()
-
+        supportFragmentManager.beginTransaction().
+            add(R.id.activity_main_frame,moviesFragment).commit()
     }
 }
---
+++ app/src/main/java/com/android/academy/fragments/MoviesFragment.kt
@@ -0,0 +1,112 @@
+package com.android.academy.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.android.academy.R
+import com.android.academy.list.MoviesAdapter
+import com.android.academy.list.OnMovieClickListener
+import com.android.academy.movie_model.MovieModel
+import kotlinx.android.synthetic.main.fragment_movies.view.*
+
+class MoviesFragment :Fragment(), OnMovieClickListener{
+    private lateinit var moviesRcv: RecyclerView
+    private lateinit var movieAdpt: MoviesAdapter
+    private val movies: MutableList<MovieModel> = ArrayList()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movies,container,false)
+        moviesRcv = view.findViewById(R.id.movies_fragment_rcv)
+        loadMovies()
+        initRecyclerView()
+
+        return view
+    }
+
+    override fun onMovieClicked(movieModel: MovieModel) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun initRecyclerView(){
+        context?.let{
+            movieAdpt = MoviesAdapter(it,this@MoviesFragment)
+            moviesRcv.adapter = movieAdpt
+            movieAdpt.setData(movies)
+        }
+    }
+
+    fun loadMovies(){
+        movies.add(
+            MovieModel(
+                "Jurassic Park",
+                R.drawable.jurassic_world_fallen_kingdom,
+                "After a volcano eruption proves to be a threat for the dinosaurs, " +
+                        "Owen and Claire reach the defunct Jurassic World, a theme park, " +
+                        "to save the animals from extinction."
+            )
+        )
+        movies.add(
+            MovieModel(
+                "The Meg",
+                R.drawable.the_meg,
+                "When the members of an underwater research facility are under threat " +
+                        "owing to a 75 feet prehistoric shark, Jonas Taylor, " +
+                        "a deep sea diver, is hired to save them."
+            )
+        )
+        movies.add(
+            MovieModel(
+                "The First Purge",
+                R.drawable.the_first_purge,
+                "To push the crime rate below one percent for the rest of the year, " +
+                        "the New Founding Fathers of America test a sociological theory that" +
+                        " vents aggression for one night in one isolated community. But when " +
+                        "the violence of oppressors meets the rage of the others."
+            )
+        )
+        movies.add(
+            MovieModel(
+                "Deadpool 2",
+                R.drawable.deadpool2,
+                "Deadpool protects a young mutant Russell from the authorities and" +
+                        " gets thrown in prison. However, he escapes and forms a team of mutants" +
+                        " to prevent a time-travelling mercenary from killing Russell."
+            )
+        )
+        movies.add(
+            MovieModel(
+                "Guardians of the Galaxy",
+                R.drawable.guardiansofthegalaxy,
+                "Peter escapes from the planet Morag with a valuable orb that Ronan" +
+                        " the Accuser wants. He eventually forms a group with unwilling heroes" +
+                        " to stop Ronan."
+            )
+        )
+        movies.add(
+            MovieModel(
+                "Ocean's 8",
+                R.drawable.oceaneight,
+                "Debbie Ocean is released from jail after serving a prison sentence." +
+                        " She assembles a special crew of seven women to steal a diamond necklace," +
+                        " worth 150 million dollars, from the Met Gala."
+            )
+        )
+        movies.add(
+            MovieModel(
+                "Thor: Ragnarok",
+                R.drawable.thor,
+                "Deprived of his mighty hammer Mjolnir, Thor must escape the" +
+                        " other side of the universe to save his home, Asgard, from Hela," +
+                        " the goddess of death."
+            )
+        )
+    }
+
+}
