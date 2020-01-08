c3c81d543141b9f620a98946e63c0ae85de03374
David Tayar <xdavidt@gmail.com>
2020-01-08 18:28:02 +0200

    add async

--- app/src/main/java/com/android/academy/AsyncActivity.kt
+++ app/src/main/java/com/android/academy/AsyncActivity.kt
@@ -1,12 +1,25 @@
 package com.android.academy
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class AsyncActivity : AppCompatActivity() {
+    companion object{
+        class CounterAsyncTask() : AsyncTask<Void,Void,Boolean>(){
+            override fun doInBackground(vararg params: Void?): Boolean {
+                var currentNum:Int
+                for(i in 1..10){
+                    currentNum = i
+                }
+                return true
+            }
 
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async)
     }
 }
+
