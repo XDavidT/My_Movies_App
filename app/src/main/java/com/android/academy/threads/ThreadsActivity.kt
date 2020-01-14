6d2fbab8d389cc0a7198ab663fa74d2241999614
xdavidt <xdavidt@gmail.com>
2020-01-14 18:34:03 +0200

    Added inherit from interface

--- app/src/main/java/com/android/academy/threads/ThreadsActivity.kt
+++ app/src/main/java/com/android/academy/threads/ThreadsActivity.kt
@@ -4,10 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.academy.R
 
-class ThreadsActivity : AppCompatActivity() {
+class ThreadsActivity : AppCompatActivity(), IAsyncTaskEvents {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_threads)
+    }
+
+    override fun onPreExecute() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPostExecute(boolean: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProgressUpdate(num: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCancel() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
