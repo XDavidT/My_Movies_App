e095daedd4c6ecb8bdab51556b10fe4a3827fdd8
xdavidt <xdavidt@gmail.com>
2020-01-15 17:40:38 +0200

    Finish thread activity.

--- app/src/main/java/com/android/academy/threads/MySimpleAsyncTask.kt
+++ app/src/main/java/com/android/academy/threads/MySimpleAsyncTask.kt
@@ -1,29 +1,70 @@
 package com.android.academy.threads
 
+import android.os.Handler
+import android.os.Looper
+
+const val THREAD_NAME = "CustomThread"
+
 class MySimpleAsyncTask(
     private val iAsyncTaskEvents: IAsyncTaskEvents
 ){
+    private var customThread: Thread? = null
+    private var isCanceld:Boolean = false
 
     fun onPreExecute(){
-
+        runOnUiThread(Runnable {
+            iAsyncTaskEvents.onPreExecute()
+        })
     }
 
     fun excute(){
-
+        runOnUiThread(Runnable {
+            onPreExecute()
+            customThread = Thread({
+                val isFinish = doInBackground()
+                runOnUiThread(
+                    Runnable {
+                        onPostExecute(isFinish)
+                    }
+                )
+            }, THREAD_NAME).also {
+                it.start()
+            }
+        })
     }
-    fun doInBackground(){
+
+    fun doInBackground():Boolean{
+        for (i in 1..10){
+            if(isCanceld) {
+                return false
+            }
+            onProgressUpdate(i)
+            Thread.sleep(1000)
+        }
+        return true
 
     }
 
     fun onProgressUpdate(num: Int) {
+        runOnUiThread(Runnable{
+            iAsyncTaskEvents.onProgressUpdate(num)
+        })
 
     }
 
     fun onPostExecute(boolean: Boolean){
-
+        runOnUiThread(Runnable{
+            iAsyncTaskEvents.onPostExecute(boolean)
+        })
     }
 
-    fun onCancel(){
+    fun cancel(){
+        isCanceld = true
+        Thread.sleep(1000)
+        customThread?.interrupt()
+    }
 
+    fun runOnUiThread(runnable: Runnable){
+        Handler(Looper.getMainLooper()).post(runnable)
     }
 }
--- app/src/main/java/com/android/academy/threads/ThreadsActivity.kt
+++ app/src/main/java/com/android/academy/threads/ThreadsActivity.kt
@@ -3,27 +3,55 @@ package com.android.academy.threads
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.academy.R
+import kotlinx.android.synthetic.main.activity_threads.*
 
 class ThreadsActivity : AppCompatActivity(), IAsyncTaskEvents {
+    private lateinit var newTask: MySimpleAsyncTask
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_threads)
+        thread_start.isEnabled = false
+
+        thread_create.setOnClickListener{
+            onCreate()
+            thread_start.isEnabled = true
+        }
+        thread_start.setOnClickListener{
+            onClickStart()
+        }
+        thread_cancel.setOnClickListener{
+            onCancel()
+        }
     }
 
     override fun onPreExecute() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        thread_start.isEnabled = false
+        thread_create.isEnabled = false
     }
 
-    override fun onPostExecute(boolean: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onPostExecute(isFinish: Boolean) {
+        if(isFinish){
+            thread_textView.text = "Done !"
+        }else{
+            thread_textView.text = "Canceled !"
+
+        }
+        thread_create.isEnabled = true
     }
 
     override fun onProgressUpdate(num: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        thread_textView.text = num.toString()
     }
 
     override fun onCancel() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        newTask.cancel()
+    }
+
+    fun onCreate(){
+        newTask = MySimpleAsyncTask(this)
+    }
+    fun onClickStart(){
+        newTask.excute()
     }
 }
