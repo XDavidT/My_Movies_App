cc0999a8858c7d5f9d4471d4325e33fd6edd4a04
xdavidt <xdavidt@gmail.com>
2020-01-21 10:54:17 +0200

    Added work manager layout

---
+++ app/src/main/java/com/android/academy/work_manager/WorkManager.kt
@@ -0,0 +1,13 @@
+package com.android.academy.work_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.academy.R
+
+class WorkManager : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_manager)
+    }
+}
